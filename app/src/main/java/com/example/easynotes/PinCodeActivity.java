d08a349eeaae277befe283b953e52829ba774079
Dmitriy25 <ohapkin_da@mail.ru>
2020-03-21 16:10:26 +0300

    Реализован экран ввода ПИН-кода

---
+++ app/src/main/java/com/example/easynotes/PinCodeActivity.java
@@ -0,0 +1,83 @@
+package com.example.easynotes;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class PinCodeActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "PinCodeActivityTag";
+
+    private String curPin = "";
+    private ArrayList<View> placeholders;
+    private TextView errorOutput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin_code);
+
+        init();
+    }
+
+    private void init() {
+        errorOutput = findViewById(R.id.errorOutput);
+
+        placeholders = new ArrayList<>();
+        placeholders.add(findViewById(R.id.placeholder1));
+        placeholders.add(findViewById(R.id.placeholder2));
+        placeholders.add(findViewById(R.id.placeholder3));
+        placeholders.add(findViewById(R.id.placeholder4));
+    }
+
+    public void onClickNumber(View view) {
+        if (!errorOutput.getText().toString().isEmpty()) {
+            errorOutput.setText("");
+        }
+
+        if (curPin.length() == 4) return;
+
+        Button curBtn = (Button) view;
+        String curNumber = curBtn.getText().toString();
+        curPin += curNumber;
+
+        placeholders.get(curPin.length() - 1).setBackground(getDrawable(R.drawable.circle_filled));
+
+        if (curPin.length() == 4) {
+            Log.d(LOG_TAG, "Делаем проверку ПИН-кода: " + curPin);
+            Keystore keystore = App.getKeystore();
+            keystore.saveNewPin("0000"); //Для тестирования
+
+            if (keystore.checkPin(curPin)) {
+                //startActivity
+                Log.d(LOG_TAG, "Переходим на главный экран с заметками");
+            } else {
+                errorOutput = findViewById(R.id.errorOutput);
+                errorOutput.setText(R.string.invalid_pin_code);
+                curPin = "";
+                for (int i = 0; i < placeholders.size(); i++) {
+                    placeholders.get(i).setBackground(getDrawable(R.drawable.circle_empty));
+                }
+            }
+        }
+
+    }
+
+    public void onClickBackspace(View view) {
+        if (curPin.length() == 0) return;
+
+        placeholders.get(curPin.length() - 1).setBackground(getDrawable(R.drawable.circle_empty));
+
+        StringBuilder tmp = new StringBuilder();
+        for (int i = 0; i < curPin.length() - 1; i++) {
+            tmp.append(curPin.charAt(i));
+        }
+        curPin = tmp.toString();
+    }
+
+}
---
+++ app/src/main/java/com/example/easynotes/SimpleKeystore.java
@@ -0,0 +1,35 @@
+package com.example.easynotes;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SimpleKeystore implements Keystore {
+    private static final String SHARED_PREF_NAME = "sharedPref";
+    private static final String PIN_CODE_KEY = "pinCodeKey";
+
+    private SharedPreferences sharedPref;
+
+    SimpleKeystore(Context context) {
+        sharedPref = context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public boolean hasPin() {
+        return (sharedPref.getString(PIN_CODE_KEY, null) != null);
+    }
+
+    @Override
+    public boolean checkPin(String pin) {
+        if (hasPin()) return sharedPref.getString(PIN_CODE_KEY, null).equals(pin);
+
+        return false;
+    }
+
+    @Override
+    public void saveNewPin(String pin) {
+        sharedPref
+                .edit()
+                .putString(PIN_CODE_KEY, pin)
+                .apply();
+    }
+}
